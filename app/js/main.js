5d6a07cb2225969602b06e3d6eba47db04f6b090
unknown <karimovMINDS@gmail.com>
2021-07-26 02:02:04 +0400

    added section video

--- app/js/main.js
+++ app/js/main.js
@@ -7,5 +7,26 @@ $(function () {
 		arrows: false
 	});
 
-	var mixer = mixitup('.products__inner');
+	// var mixer = mixitup('.design__inner');
+	// var mixer = mixitup('.products__inner');
+
+	// var container1 = document.querySelector('[data-ref="products]');
+	// var container2 = document.querySelector('[data-ref="design]');
+
+	// var mixer1 = mixitup(container1);
+	// var mixer2 = mixitup(container2);
+
+	$('.design__inner').mixitup({
+		selectors: {
+			filter: '.design__filter'
+		}
+	});
+
+	$('.products__inner').mixitup({
+		selectors: {
+			filter: '.products__filter'
+		}
+	});
+
+	// var mixer1 = mixitup("$('[data-ref="products"]')")
 });
