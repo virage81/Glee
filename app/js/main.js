26ba666a4c75ea07af2d3ca18f3ef7f92f200baf
unknown <karimovMINDS@gmail.com>
2021-07-31 04:24:24 +0400

    added product list1/2 TODO: fix link in items on product list1!!!

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,43 @@
 $(function () {
 	/*
+============= Ion-rangeSlider ==================
+	*/
+	$('.filter-price__input').ionRangeSlider({
+		type: 'double',
+		prefix: '$',
+		step: '0.01',
+		onStart: function (data) {
+			$('.filter-price__from').text(data.from + '.00');
+			$('.filter-price__to').text(data.to + '.00');
+		},
+		onChange: function (data) {
+			$('.filter-price__from').text(data.from);
+			$('.filter-price__to').text(data.to);
+		},
+	});
+
+	/*
+============= Ion-rangeSlider ==================
+	*/
+	// $('.recent-product__item-star').rateYo({
+	// 
+	// });
+
+	$('.products__item-info__star').rateYo({
+		starWidth: '18px',
+		normalFill: '#d6d6d6',
+		ratedFill: '#ffcc00',
+		readOnly: true,
+	});
+
+	$('.recent-product__item-star').rateYo({
+		starWidth: '11px',
+		normalFill: '#d6d6d6',
+		ratedFill: '#ffcc00',
+		readOnly: true,
+	});
+
+	/*
 ============= Slick-Slider ==================
 	*/
 	$('.top-slider__inner').slick({
--- gulpfile.js
+++ gulpfile.js
@@ -32,11 +32,15 @@ function styles() {
 }
 
 function scripts() {
-	return src(['node_modules/jquery/dist/jquery.js',
-	'node_modules/mixitup/dist/mixitup.js',
-	'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js',
-	'node_modules/slick-carousel/slick/slick.js',
-	'app/js/main.js'])
+	return src([
+		'node_modules/jquery/dist/jquery.js',
+		'node_modules/mixitup/dist/mixitup.js',
+		'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js',
+		'node_modules/ion-rangeslider/js/ion.rangeSlider.js',
+		'node_modules/rateyo/src/jquery.rateyo.js',
+		'node_modules/slick-carousel/slick/slick.js',
+		'app/js/main.js',
+	])
 		.pipe(concat('main.min.js'))
 		.pipe(uglify())
 		.pipe(dest('app/js'))
