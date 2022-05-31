59c4b123c50227539bb6777f2a0268d8a802eae3
Bulat <karimovMINDS@gmail.com>
2022-05-31 17:21:56 +0400

    added icons to blog section

--- gulpfile.js
+++ gulpfile.js
@@ -1,68 +1,65 @@
-const { src, dest, watch, parallel, series } = require('gulp');
+const { src, dest, watch, parallel, series } = require("gulp");
 
-const scss = require('gulp-sass')(require('sass'));
-const concat = require('gulp-concat');
-const autoprefixer = require('gulp-autoprefixer');
-const uglify = require('gulp-uglify');
-const imagemin = require('gulp-imagemin');
-const rename = require('gulp-rename');
-const nunjucksRender = require('gulp-nunjucks-render');
-const browserSync = require('browser-sync').create();
-const del = require('del');
+const scss = require("gulp-sass")(require("sass"));
+const concat = require("gulp-concat");
+const autoprefixer = require("gulp-autoprefixer");
+const uglify = require("gulp-uglify");
+const imagemin = require("gulp-imagemin");
+const rename = require("gulp-rename");
+const nunjucksRender = require("gulp-nunjucks-render");
+const browserSync = require("browser-sync").create();
+const del = require("del");
 
 function browsersync() {
 	browserSync.init({
 		server: {
-			baseDir: 'app/',
+			baseDir: "app/",
 		},
 		notify: false,
 	});
 }
 
 function nunjucks() {
-	return src('app/njk/*.njk')
-	.pipe(nunjucksRender())
-	.pipe(dest('app'))
-	.pipe(browserSync.stream());
+	return src("app/njk/*.njk").pipe(nunjucksRender()).pipe(dest("app")).pipe(browserSync.stream());
 }
 
 function styles() {
-	return src('app/scss/*.scss')
-		.pipe(scss({ outputStyle: 'compressed' }))
+	return src("app/scss/*.scss")
+		.pipe(scss({ outputStyle: "compressed" }))
 		.pipe(
 			rename({
-				suffix: '.min',
+				suffix: ".min",
 			})
 		)
 		.pipe(
 			autoprefixer({
-				overrideBrowserslist: ['last 10 versions'],
+				overrideBrowserslist: ["last 10 versions"],
 				grid: true,
 			})
 		)
-		.pipe(dest('app/css'))
+		.pipe(dest("app/css"))
 		.pipe(browserSync.stream());
 }
 
 function scripts() {
 	return src([
-		'node_modules/jquery/dist/jquery.js',
-		'node_modules/mixitup/dist/mixitup.js',
-		'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js',
-		'node_modules/ion-rangeslider/js/ion.rangeSlider.js',
-		'node_modules/rateyo/src/jquery.rateyo.js',
-		'node_modules/slick-carousel/slick/slick.js',
-		'node_modules/jquery-form-styler/dist/jquery.formstyler.js',
-		'app/js/main.js',
+		"node_modules/jquery/dist/jquery.js",
+		"node_modules/mixitup/dist/mixitup.js",
+		"node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js",
+		"node_modules/ion-rangeslider/js/ion.rangeSlider.js",
+		"node_modules/rateyo/src/jquery.rateyo.js",
+		"node_modules/slick-carousel/slick/slick.js",
+		"node_modules/jquery-form-styler/dist/jquery.formstyler.js",
+		"app/js/main.js",
 	])
-		.pipe(concat('main.min.js'))
+		.pipe(concat("main.min.js"))
 		.pipe(uglify())
-		.pipe(dest('app/js'))
+		.pipe(dest("app/js"))
 		.pipe(browserSync.stream());
 }
 
 function images() {
-	return src('app/img/**/*.*')
+	return src("app/img/**/*.*")
 		.pipe(
 			imagemin([
 				imagemin.gifsicle({ interlaced: true }),
@@ -73,26 +70,22 @@ function images() {
 				}),
 			])
 		)
-		.pipe(dest('dist/img'));
+		.pipe(dest("dist/img"));
 }
 
 function build() {
-	return src([
-		'app/**/*.html',
-		'app/css/*.min.css',
-		'app/js/main.min.js',
-		'app/fonts/*.*'], { base: 'app' }).pipe(dest('dist/'));
+	return src(["app/**/*.html", "app/css/*.min.css", "app/js/main.min.js", "app/fonts/*.*"], { base: "app" }).pipe(dest("dist/"));
 }
 
 function cleanDist() {
-	return del('dist/');
+	return del("dist/");
 }
 
 function watching() {
-	watch(['app/**/*.scss'], styles);
-	watch(['app/njk/*.njk', 'app/module/**/*.html'], nunjucks);
-	watch(['app/js/**/*.js', '!app/js/main.min.js'], scripts);
-	watch(['app/**/*.html']).on('change', browserSync.reload);
+	watch(["app/**/*.scss"], styles);
+	watch(["app/njk/*.njk", "app/module/*/*.*", "app/module/**/*.html"], nunjucks);
+	watch(["app/js/**/*.js", "!app/js/main.min.js"], scripts);
+	watch(["app/**/*.html"]).on("change", browserSync.reload);
 }
 
 exports.styles = styles;
