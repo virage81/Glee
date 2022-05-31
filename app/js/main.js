bf69f83fd98de1260b59b8b53fa6c1d6fdfa8053
Bulat <karimovMINDS@gmail.com>
2022-05-31 20:07:39 +0400

    added cosmetic things, fixed texts and changed visibility

--- app/js/main.js
+++ app/js/main.js
@@ -2,49 +2,49 @@ $(function () {
 	/*
 ============= Filter Button ==================
 	*/
-	$('.shop-filters__btn').on('click', function () {
-		$('.shop-filters').toggleClass('shop-filters--active');
+	$(".shop-filters__btn").on("click", function () {
+		$(".shop-filters").toggleClass("shop-filters--active");
 	});
 
 	/*
 ============= Menu Button ==================
 	*/
-	$('.user-nav__link--menu').on('click', function () {
-		$('.menu').toggleClass('menu--active');
+	$(".user-nav__link--menu").on("click", function () {
+		$(".menu").toggleClass("menu--active");
 	});
 
 	/*
 ============= Product Tabs ==================
 	*/
-	$('.product-tabs__top-item').on('click', function (e) {
+	$(".product-tabs__top-item").on("click", function (e) {
 		e.preventDefault();
 
-		$('.product-tabs__top-item').removeClass('product-tabs__top-item--active');
-		$(this).addClass('product-tabs__top-item--active');
+		$(".product-tabs__top-item").removeClass("product-tabs__top-item--active");
+		$(this).addClass("product-tabs__top-item--active");
 
-		$('.product-tabs__content-item').removeClass('product-tabs__content-item--active');
-		$($(this).attr('href')).addClass('product-tabs__content-item--active');
+		$(".product-tabs__content-item").removeClass("product-tabs__content-item--active");
+		$($(this).attr("href")).addClass("product-tabs__content-item--active");
 	});
 
 	/*
 ============= Form Styler ==================
 	*/
-	$('.product-one__info-input').styler({});
+	$(".product-one__info-input").styler({});
 
 	/*
 ============= Ion-rangeSlider ==================
 	*/
-	$('.filter-price__input').ionRangeSlider({
-		type: 'double',
-		prefix: '$',
-		step: '0.01',
+	$(".filter-price__input").ionRangeSlider({
+		type: "double",
+		prefix: "$",
+		step: "0.01",
 		onStart: function (data) {
-			$('.filter-price__from').text(data.from + '.00');
-			$('.filter-price__to').text(data.to + '.00');
+			$(".filter-price__from").text(data.from + ".00");
+			$(".filter-price__to").text(data.to + ".00");
 		},
 		onChange: function (data) {
-			$('.filter-price__from').text(data.from);
-			$('.filter-price__to').text(data.to);
+			$(".filter-price__from").text(data.from);
+			$(".filter-price__to").text(data.to);
 		},
 	});
 
@@ -52,32 +52,33 @@ $(function () {
 ============= rateYo ==================
 	*/
 
-	$('.products__item-info__star').rateYo({
-		starWidth: '18px',
-		normalFill: '#d6d6d6',
-		ratedFill: '#ffcc00',
+	$(".products__item-info__star").rateYo({
+		starWidth: "18px",
+		normalFill: "#d6d6d6",
+		ratedFill: "#ffcc00",
 		readOnly: true,
-		spacing: '12px',
+		spacing: "12px",
 	});
 
-	$('.recent-product__item-star').rateYo({
-		starWidth: '11px',
-		normalFill: '#d6d6d6',
-		ratedFill: '#ffcc00',
+	$(".recent-product__item-star").rateYo({
+		starWidth: "11px",
+		normalFill: "#d6d6d6",
+		ratedFill: "#ffcc00",
 		readOnly: true,
 	});
 
-	$('.product-one__info-star').rateYo({
-		starWidth: '18px',
-		normalFill: '#d6d6d6',
-		ratedFill: '#ffcc00',
+	$(".product-one__info-star").rateYo({
+		starWidth: "18px",
+		normalFill: "#d6d6d6",
+		ratedFill: "#ffcc00",
 		readOnly: true,
 	});
 
 	/*
 ============= Slick-Slider ==================
 	*/
-	$('.top-slider__inner').slick({
+
+	$(".top-slider__inner").slick({
 		dots: true,
 		fade: true,
 		autoplay: 3000,
@@ -85,8 +86,8 @@ $(function () {
 		arrows: false,
 	});
 
-	$('.product-one__slider-thumb').slick({
-		asNavFor: '.product-one__slider-big',
+	$(".product-one__slider-thumb").slick({
+		asNavFor: ".product-one__slider-big",
 		slidesToShow: 3,
 		slidesToScroll: 1,
 		arrows: false,
@@ -95,7 +96,7 @@ $(function () {
 		draggable: false,
 	});
 
-	$('.product-one__slider-big').slick({
+	$(".product-one__slider-big").slick({
 		slidesToShow: 1,
 		arrows: false,
 		fade: true,
@@ -119,7 +120,7 @@ $(function () {
 
 	var config = {
 		controls: {
-			scope: 'local',
+			scope: "local",
 		},
 	};
 
